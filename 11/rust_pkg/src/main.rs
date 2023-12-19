7111ed70dac29c7f2c7ca88cb741716adc77663b
Your Name <youremail@yourdomain.com>
2023-12-19 14:09:37 +0100

    11 desember part2

--- 11/rust_pkg/src/main.rs
+++ 11/rust_pkg/src/main.rs
@@ -4,12 +4,12 @@ My first ever Rust code ;)
 */
 
 use core::cmp::max as max;
-// use std::fs;
-// use regex::Regex;
+
+const IS_PART1 : bool = false;
 
 // Find 2D coordinates of '#' in the INPUT
-fn find_galaxies (input : &[u8]) -> Vec::<[u32; 2]> {
-    let mut galaxies = Vec::<[u32; 2]>::new();
+fn find_galaxies (input : &[u8]) -> Vec::<[u64; 2]> {
+    let mut galaxies = Vec::<[u64; 2]>::new();
     let mut row = 0;
     let mut col = 0;
     for c in input {
@@ -31,36 +31,25 @@ fn find_galaxies (input : &[u8]) -> Vec::<[u32; 2]> {
 
 // Correct galaxy coordinate for universe expansion
 // \param dim: dimension (row or col)
-fn expand_universe(mut galaxies: Vec::<[u32; 2]>, dim : usize) -> Vec::<[u32; 2]> {
-    let universe_dimension = galaxies.first().expect("No galaxies in universe :/").len();
-    assert!(dim < universe_dimension); // 2D
+fn expand_universe(mut galaxies: Vec::<[u64; 2]>, dim : usize) -> Vec::<[u64; 2]> {
+    let expansion_size = if IS_PART1 {1} else {1000000-1};
 
     galaxies.sort_by_key(|g| g[dim]);
 
     let mut last = galaxies[0][dim];
     let mut expansion = 0;
-
     for (_index, g) in galaxies.iter_mut().enumerate() {
         expansion += max(g[dim] - last, 1) - 1;
         last = g[dim];
-        g[dim] += expansion;
+        g[dim] += expansion * expansion_size;
     }
     return galaxies;
 }
 
 fn main() {
-    // const FILENAME = String::from();
-    // const INPUT : &[u8; 118] = include_bytes!("input_test.txt");
     let input = include_bytes!("input.txt");
 
     let mut galaxies =  find_galaxies(input);
-    
-    // println!("___________________________");
-    // {
-    //     for g in &galaxies {
-    //         println!("({}, {})", g[0], g[1]);
-    //     }
-    // }
     
     galaxies = expand_universe(galaxies, 0);
     galaxies = expand_universe(galaxies, 1);
@@ -71,8 +60,6 @@ fn main() {
         }).sum::<u64>()
     }).sum::<u64>();
 
-    println!("part1: {}", distances);
-
-    // std::thread::sleep(std::time::Duration::from_millis(4000));
+    println!("part {}: {}", if IS_PART1 {"1"} else {"2"}, distances);
 }
   
